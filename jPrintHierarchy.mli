0e7d90975b27d6cd87f7b5227fc61ebb1ad0106c
Laurent Hubert <lhubert@51fa54ed-fb29-0410-ab4c-e7ce538f1b83>
2008-08-04 16:17:43 +0000

    * adding a missing header

--- jPrintHierarchy.mli
+++ jPrintHierarchy.mli
@@ -1,3 +1,23 @@
+(*
+ *  This file is part of JavaLib
+ *  Copyright (c)2007 Université de Rennes 1 / CNRS
+ *  Tiphaine Turpin <first.last@irisa.fr>
+ *  Laurent Hubert <first.last@irisa.fr>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *)
 
 (** [get_hierachy prog info] returns [info] enriched with information
     from the hierachy such as subclasses, subinterfaces and
