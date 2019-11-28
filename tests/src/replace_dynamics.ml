5e7f1bb5387bc54f0e479c9a34f62bfb2eec1b99
Nicolas Barré <nicolas.barre@irisa.fr>
2019-11-28 10:09:39 +0100

    Correction

--- tests/src/replace_dynamics.ml
+++ tests/src/replace_dynamics.ml
@@ -5,7 +5,7 @@ open JBasics
 let dump_class classpath scname prefix dest =
   let cname = make_cn scname in
   let ioc = get_class classpath cname in
-  let (ioc', cmap) = remove_invokedynamics ioc prefix in
+  let (ioc', cmap) = remove_invokedynamics ioc ~prefix in
   let dest_main =  Printf.sprintf "%s/%s.class" dest scname in
   let ch = open_out_bin dest_main in
   unparse_class ioc' ch;
