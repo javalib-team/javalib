84103ca68b6f97c8b26e75064c71430ea043b9c9
David Pichardie <dpichardie@fb.com>
2023-01-05 14:25:47 +0100

    remove Container dependency

--- src/jLib.ml
+++ src/jLib.ml
@@ -31,11 +31,25 @@ end
                  
 module List = struct
 
-  include CCList (* Containers *)
-
-  let split_nth = take_drop
+  include List
+  
+  let rec drop n l  =
+    match l with
+    | _ :: l when n > 0 -> drop (n-1) l
+    | _ -> l
 
   let remove_all l x = filter (fun y -> x<>y) l
+
+  exception Invalid_index of int
+      
+  let split_nth n l =
+    let rec aux i l1 l2 =
+      match i, l2 with
+      | 0, _ -> (List.rev l1, l2)
+      | _, [] -> raise (Invalid_index n)
+      | _, x :: l2 -> aux (i-1) (x::l1) l2
+    in aux 0 [] l
+    
        
 end                  
 
