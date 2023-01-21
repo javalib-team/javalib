javalib-team/javalib

commit 84103ca68b6f97c8b26e75064c71430ea043b9c9

remove Container dependency

src/jLib.ml
@@ -31,11 +31,25 @@ end
                  
 module List = struct
 
-  include CCList (* Containers *)
-
-  let split_nth = take_drop
+  include List
+  
+  let rec drop n l  =
+    match l with
+    | _ :: l when n > 0 -> drop (n-1) l
+    | _ -> l
 
   let remove_all l x = filter (fun y -> x<>y) l
+
+  exception Invalid_index of int
+      
+  let split_nth n l =
+    let rec aux i l1 l2 =
+      match i, l2 with
+      | 0, _ -> (List.rev l1, l2)
+      | _, [] -> raise (Invalid_index n)
+      | _, x :: l2 -> aux (i-1) (x::l1) l2
+    in aux 0 [] l
+    
        
 end                  
 

javalib.opam
@@ -18,7 +18,6 @@ depends: [
   "ocamlfind" {build}
   "camlzip" {>= "1.11"}
   "extlib"
-  "containers"
 ]
 
 synopsis: "Javalib is a library written in OCaml with the aim to provide a high level representation of Java .class files"

commit d87d44ca0d024c1f0084c588186cbdf765d95afe

Fix a regression in List.split_nth impl
Summary:
The regression was introduced in https://github.com/javalib-team/javalib/commit/84103ca68b6f97c8b26e75064c71430ea043b9c9

In particular it causes infer to eithe spin indefinitely or simply
produce wrong results.

This PR fixes a bug, but NOTE that the new implementation has a
different semantics from that of `take_drop` in Collections.

Test Plan:
Tested manually. Would be nice to add some unit tests but perhaps for
another time.

src/jLib.ml
@@ -48,7 +48,7 @@ module List = struct
       | 0, _ -> (List.rev l1, l2)
       | _, [] -> raise (Invalid_index n)
       | _, x :: l2 -> aux (i-1) (x::l1) l2
-    in aux 0 [] l
+    in aux n [] l
     
        
 end                  