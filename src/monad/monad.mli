584fa4c1bbf4ae10f5a46105b3df1b7539679a0e
Alban Dutilleul <alban.dutilleul@ens-rennes.fr>
2023-02-07 15:55:28 +0100

    [code style]: add mli interface for state monad

---
+++ src/monad/monad.mli
@@ -0,0 +1,22 @@
+module State :
+  sig
+    type ('s, 'a) t = State of ('s -> 's * 'a)
+    val return : 'a -> ('b, 'a) t
+    val fmap : ('a -> 'b) -> ('c, 'a) t -> ('c, 'b) t
+    val bind : ('a, 'b) t -> ('b -> ('a, 'c) t) -> ('a, 'c) t
+    val seq : ('a, 'b) t -> ('a, 'c) t -> ('a, 'c) t
+    val get : unit -> ('a, 'a) t
+    val set : 'a -> ('a, unit) t
+    val modify : ('a -> 'a) -> ('a, unit) t
+    val run : ('a, 'b) t -> 'a -> 'a * 'b
+    val exec : ('a, 'b) t -> 'a -> 'b
+    val fold_leftM : ('a -> 'b -> ('c, 'a) t) -> 'a -> 'b list -> ('c, 'a) t
+    module Infix :
+      sig
+        val ( <$> ) : ('a -> 'b) -> ('c, 'a) t -> ('c, 'b) t
+        val ( >>= ) : ('a, 'b) t -> ('b -> ('a, 'c) t) -> ('a, 'c) t
+        val ( =<< ) : ('a -> ('b, 'c) t) -> ('b, 'a) t -> ('b, 'c) t
+        val ( >> ) : ('a, 'b) t -> ('a, 'c) t -> ('a, 'c) t
+        val ( let* ) : ('a, 'b) t -> ('b -> ('a, 'c) t) -> ('a, 'c) t
+      end
+  end
