cda4c2fd1841d70cbb026d3c1391c0f8d8f75f0c
Martin Andrieux <martin.andrieux@ens-rennes.fr>
2023-03-01 16:45:23 +0100

    hide useless module

--- src/seaofnodes/seaOfNodes__Translator.mli
+++ src/seaofnodes/seaOfNodes__Translator.mli
@@ -19,25 +19,4 @@
  * <http://www.gnu.org/licenses/>.
  *)
 
-module TranslatorState : sig
-  type t = {stack: SeaOfNodes__Type.Data.t list; region: SeaOfNodes__Type.Region.t; count: int}
-
-  type 'a monad = (t, 'a) Monad.State.t
-
-  val initial : t
-
-  val push_stack : SeaOfNodes__Type.Data.t -> unit monad
-
-  val pop_stack : unit -> SeaOfNodes__Type.Data.t monad
-
-  val fresh : unit -> int monad
-
-  val get_current_region : unit -> SeaOfNodes__Type.Region.t monad
-end
-
-val translate_jopcode :
-     SeaOfNodes__Type.Son.t
-  -> JCode.jopcode
-  -> (TranslatorState.t, SeaOfNodes__Type.Son.t) Monad.State.t
-
 val translate_jopcodes : JCode.jopcodes -> SeaOfNodes__Type.Son.t
