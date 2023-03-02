aec3c7e9afa92add54dc2c7e7dfa5d56697c8841
Alban Dutilleul <alban.dutilleul@ens-rennes.fr>
2023-03-02 08:58:42 +0100

    refactor: make separate lib for shared methods across son tests

--- tests/src/son-tests/son_test.ml
+++ tests/src/son-tests/son_test.ml
@@ -1,32 +1,5 @@
-open Javalib
-open JBasics
-open JCode
 open SeaOfNodes
-
-let get_jopcodes c =
-  match c with
-  | JInterface _ ->
-      assert false
-  | JClass _ -> (
-      let methods = get_methods c in
-      let m = List.hd @@ MethodMap.value_elements methods in
-      match m with
-      | AbstractMethod _ ->
-          assert false
-      | ConcreteMethod cm -> (
-        match cm.cm_implementation with
-        | Native ->
-            assert false
-        | Java code ->
-            let jcode = Lazy.force code in
-            jcode.c_code ) )
-
-let rec map_option (f : 'a -> 'b option) (l : 'a list) : 'b list =
-  match l with
-  | [] ->
-      []
-  | hd :: tl -> (
-    match f hd with None -> map_option f tl | Some hd -> hd :: map_option f tl )
+open Utils
 
 let _ =
   let cp = Javalib.class_path "java_files" in
@@ -51,5 +24,5 @@ let _ =
     map_option (function SeaOfNodes.Type.Node.Data data -> Some data | _ -> None) nodes
   in
   assert (
-    List.for_all (fun a -> List.for_all (fun n -> not (a = n && a != n)) data_nodes) data_nodes );
+    List.for_all (fun a -> List.for_all (fun n -> not (a = n && a != n)) data_nodes) data_nodes ) ;
   Printf.printf "Test passed successfully.\n"
